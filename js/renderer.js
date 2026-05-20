41215345ec8422165436d7c912a4169dd832bedf
yum <wanquanbuhuime@163.com>
2026-05-21 01:35:58 +0800

    Fix WGSL recursive error: update embedded SHADER_CODE to iterative layout

--- js/renderer.js
+++ js/renderer.js
@@ -504,57 +504,7 @@ fn parse_all_css(
   }
 }
 
-fn layout_node_children(
-  dom_tree: ptr<storage, DomRootTree, read_write>,
-  parent_idx: u32,
-  visited: ptr<function, array<bool, 512>>
-) {
-  var parent = &(*dom_tree).nodes[parent_idx];
-  if ((*visited)[parent_idx]) { return; }
-  (*visited)[parent_idx] = true;
-
-  var current_x = (*parent).style.layout_x + (*parent).style.padding_left;
-  var current_y = (*parent).style.layout_y + (*parent).style.padding_top;
-
-  var child_idx = (*parent).first_child;
-  while (child_idx != 512u && child_idx < (*dom_tree).total_node) {
-    var child = &(*dom_tree).nodes[child_idx];
-
-    if ((*child).style.is_text_node) {
-      if ((*child).style.content_w <= 0.0 || (*child).style.content_h <= 0.0) {
-        (*child).style.skip_render = true;
-      }
-    }
-
-    if (!(*child).style.skip_render) {
-      var child_w = select((*child).style.content_w, (*child).style.width, (*child).style.width > 0.0);
-      var child_h = select((*child).style.content_h, (*child).style.height, (*child).style.height > 0.0);
-
-      if (child_w <= 0.0) { child_w = 100.0; }
-      if (child_h <= 0.0) { child_h = 20.0; }
-
-      (*child).style.content_w = child_w;
-      (*child).style.content_h = child_h;
-
-      (*child).style.layout_x = current_x + (*child).style.margin_left;
-      (*child).style.layout_y = current_y + (*child).style.margin_top;
-
-      if ((*child).style.is_block) {
-        current_y += child_h + (*child).style.margin_top + 8.0;
-      } else {
-        current_x += child_w + (*child).style.margin_left + 4.0;
-      }
-    }
-
-    if ((*child).first_child != 512u) {
-      layout_node_children(dom_tree, child_idx, visited);
-    }
-
-    child_idx = (*child).next_sibling;
-  }
-}
-
-fn calculate_layout(
+fn layout_iterative(
   dom_tree: ptr<storage, DomRootTree, read_write>
 ) {
   var root = &(*dom_tree).nodes[(*dom_tree).root_node_id];
@@ -563,12 +513,65 @@ fn calculate_layout(
   (*root).style.content_w = CANVAS_W - 20.0;
   (*root).style.content_h = CANVAS_H - 20.0;
 
-  var visited: array<bool, 512>;
-  for (var i: u32 = 0u; i < 512u; i++) {
-    visited[i] = false;
-  }
+  var stack: array<u32, 64>;
+  var stack_size: u32 = 0u;
 
-  layout_node_children(dom_tree, (*dom_tree).root_node_id, &visited);
+  stack[0] = (*dom_tree).root_node_id;
+  stack_size = 1u;
+
+  while (stack_size > 0u) {
+    stack_size = stack_size - 1u;
+    var parent_idx = stack[stack_size];
+
+    var parent = &(*dom_tree).nodes[parent_idx];
+
+    var current_x = (*parent).style.layout_x + (*parent).style.padding_left;
+    var current_y = (*parent).style.layout_y + (*parent).style.padding_top;
+
+    var child_idx = (*parent).first_child;
+    while (child_idx != 512u && child_idx < (*dom_tree).total_node) {
+      var child = &(*dom_tree).nodes[child_idx];
+
+      if ((*child).style.is_text_node) {
+        if ((*child).style.content_w <= 0.0 || (*child).style.content_h <= 0.0) {
+          (*child).style.skip_render = true;
+        }
+      }
+
+      if (!(*child).style.skip_render) {
+        var child_w = select((*child).style.content_w, (*child).style.width, (*child).style.width > 0.0);
+        var child_h = select((*child).style.content_h, (*child).style.height, (*child).style.height > 0.0);
+
+        if (child_w <= 0.0) { child_w = 100.0; }
+        if (child_h <= 0.0) { child_h = 20.0; }
+
+        (*child).style.content_w = child_w;
+        (*child).style.content_h = child_h;
+
+        (*child).style.layout_x = current_x + (*child).style.margin_left;
+        (*child).style.layout_y = current_y + (*child).style.margin_top;
+
+        if ((*child).style.is_block) {
+          current_y += child_h + (*child).style.margin_top + 8.0;
+        } else {
+          current_x += child_w + (*child).style.margin_left + 4.0;
+        }
+      }
+
+      if ((*child).first_child != 512u && stack_size < 64u) {
+        stack[stack_size] = child_idx;
+        stack_size = stack_size + 1u;
+      }
+
+      child_idx = (*child).next_sibling;
+    }
+  }
+}
+
+fn calculate_layout(
+  dom_tree: ptr<storage, DomRootTree, read_write>
+) {
+  layout_iterative(dom_tree);
 }
 
 fn render_pixel(
